b05ed052032e5485f586a8690ce72bcd77208ef4
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-11-10 23:23:15 +0100

    get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Get, Param, Controller, Post } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -8,5 +8,10 @@ export class UsersController {
   @Post('signup')
   createUser(@Body() body: createUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    this.usersService.findOne(parseInt(id));
   }
 }
