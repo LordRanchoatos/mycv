f4ded6b01c15eaeab809476889a0e3d362b965c3
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-11-17 16:22:22 +0100

    handle other req on user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Get, Param, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Get,
+  Param,
+  Controller,
+  Post,
+  // Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -7,11 +16,25 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('signup')
   createUser(@Body() body: createUserDto) {
+    console.log('C', body.email, body.password);
     this.usersService.create(body.email, body.password);
   }
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
-    this.usersService.findOne(parseInt(id));
+    return this.usersService.findOne(parseInt(id));
   }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+  
+  // @Patch()
+
 }
